f0001bfd4fcf36476a601f7199d58620cb4986e0
NicoMalta <niicou96@hotmail.com>
2017-04-12 20:09:58 -0300

    TP 0.01

---
+++ TheBestTruco/TheBestTruco.Entidades/Carta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestTruco.Entidades
+{
+    class Carta
+    {
+        public string Palo { get; set; }
+        public int Numero { get; set; }
+        public int Valor { get; set; }
+    }
+}
---
+++ TheBestTruco/TheBestTruco.Entidades/Mazo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestTruco.Entidades
+{
+    class Mazo
+    {
+        List<Carta> mazo { get; set; }
+
+        Mazo()
+        {
+            new List<Carta>();
+        }
+    }
+}
