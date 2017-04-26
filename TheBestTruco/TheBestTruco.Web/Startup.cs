f65ef6a86fda4f7a32146bf548174fe2b9884f1f
NicoMalta <niicou96@hotmail.com>
2017-04-26 20:13:59 -0300

    "TP 0.14"

---
+++ TheBestTruco/TheBestTruco.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(TheBestTruco.Web.Startup))]
+namespace TheBestTruco.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
